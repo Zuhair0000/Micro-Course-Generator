ee97bcd4549092bb0c7a8df3b82dd7549596e203
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-10-22 19:34:22 +0800

    setting up backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import AuthForm from "../components/AuthForm";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async (formdata) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${API_URL}/api/auth/login`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.error || "Login failed");
+
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      navigate("/dashboard");
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen bg-[#1F2028]">
       <Navbar />
 
       <div className="flex justify-center items-center mt-20 px-4">
-        <AuthForm type="login" />
+        <AuthForm type="login" onSubmit={handleLogin} isLoading={isLoading} />
       </div>
     </div>
   );
