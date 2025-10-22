677f4e451aa1205c338a16de76b15cdc213dbf76
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-10-22 19:22:03 +0800

    setting up backend

---
+++ backend/controllers/authController.js
@@ -0,0 +1,82 @@
+const pool = require("../db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    hashedPassword = await bcrypt.hash(password, 10);
+
+    const usersResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (usersResult.rows.length > 0) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const user = await pool.query(
+      "INSERT INTO users(name, email, password) VALUES ($1, $2, $3)",
+      [name, email, hashedPassword]
+    );
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: user.rows[0] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "all fields are required" });
+  }
+
+  try {
+    const usersResult = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (usersResult.rows.length === 0) {
+      res.status(400).json("User does not exist");
+    }
+
+    const user = usersResult.rows[0];
+
+    const isMatched = await bcrypt.compare(password, user.password);
+
+    if (!isMatched) {
+      return res.status(400).json({ message: "password is wrong" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(201).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
